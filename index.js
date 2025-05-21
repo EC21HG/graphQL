34d417310ed0ee4f33742372a83744d3014a34c0
ecampin <evco214@proton.me>
2025-05-21 16:31:47 +0200

    V1.2

--- index.js
+++ index.js
@@ -1,29 +1,35 @@
-const graphEnd = 'https://zone01normandie.org/api/graphql-engine/v1/graphql';
-const form = document.getElementById('form');
+const form = document.getElementById('login-form');
 const result = document.getElementById('result');
 
 form.addEventListener('submit', async (e) => {
   e.preventDefault();
 
-  const email = document.getElementById('email').value;
-  const password = document.getElementById('password').value;
+  const text = document.getElementById('text').value.trim();
+  const password = document.getElementById('password').value.trim();
+  const credentials = btoa(`${text}:${password}`)
 
-  // Étape 1 : Authentification
   const signinR = await fetch('https://zone01normandie.org/api/auth/signin', {
     method: 'POST',
-    headers: { 'Content-Type': 'application/json' },
-    body: JSON.stringify({ email, password })
+    headers: { 
+      'Authorization': `Basic ${credentials}`,
+      'Content-Type': 'application/json' 
+    },
+    body: JSON.stringify({ text, password })
   });
 
   if (!signinR.ok) {
-    result.textContent = "Auth Failed";
-    return;
-  }
+  const errorText = await signinR.text();
+  console.error("Erreur AUTH", signinR.status, errorText);
+  result.textContent = `Erreur d'authentification (${signinR.status}) : ${errorText}`;
+  return;
+}
 
-  const { token } = await signinR.json();
+let token = await signinR.text();
+token = token.replace(/^"|"$/g, '').trim();
+console.log("Auth:",token)
 
-  // Étape 2 : Requête GraphQL
-  const graphqlR = await fetch(graphEnd, {
+
+  const graphqlR = await fetch('https://zone01normandie.org/api/graphql-engine/v1/graphql', {
     method: 'POST',
     headers: {
       'Content-Type': 'application/json',
@@ -32,49 +38,73 @@ form.addEventListener('submit', async (e) => {
     body: JSON.stringify({
       query: `
         query {
-          user {
-            login
-            transactions(
-              where: { eventId: { _eq: 148 } }
-              order_by: { createdAt: asc }
-            ) {
-              amount
-              type
-              createdAt
+            user{
+                id 
+                login
+                attrs
+                totalUp
+                totalDown
+                transactions ( where: {eventId: {_eq: 148}}, order_by: {createdAt:asc}){
+                amount
+                type
+                createdAt
+                }
             }
-          }
-          event_user(
-            where: { userId: { _eq: 3634 }, eventId: { _eq: 303 } }
-          ) {
-            level
-          }
+          event_user(where: { userId: { _eq: 3634 }, eventId: { _eq: 303 } }) {
+                      level
+                  }
         }
       `
     })
   });
 
   const graphqlData = await graphqlR.json();
+  const user = graphqlData.data.user[0]
+  const transactions = user.transactions
+  const level = graphqlData.data.event_user[0]?.level
   result.textContent = JSON.stringify(graphqlData, null, 2);
+  form.classList.add('hidden')
+  const card = document.getElementById('result')
+  card.classList.remove('hidden')
+
+
+  const html = `
+  <div class="card-inner">
+  <h2>Hi ${user.firstName}</h2>
+  <p>User : ${user.login}</p>
+  <p> <strong> Total Up :</strong> ${user.totalUp}</p>
+  <p> <strong> Total Down:</strong> ${user.totalDown}</p>
+  <p> <strong> Your actual lvl is :</strong> ${level}</p>
+  <h3> Transactions (eventId: 148) </h3>
+  <ul> ${transactions.map(tx => `
+    <li>
+    <strong> ${tx.type}</strong> : ${tx.amount} (today ${new Date(tx.createdAt).toLocaleDateString()})
+    </li>
+    `).join('')}
+    </ul>
+  </div>
+  `
+  result.innerHTML = html
 });
 
 
-const query = `
-  query {
-    user {
-      login
-      transactions(
-        where: { eventId: { _eq: 148 } }
-        order_by: { createdAt: asc }
-      ) {
-        amount
-        type
-        createdAt
-      }
-    }
-    event_user(
-      where: { userId: { _eq: 3634 }, eventId: { _eq: 303 } }
-    ) {
-      level
-    }
-  }
-`;
+// const query = `
+//   query {
+//     user {
+//       login
+//       transactions(
+//         where: { eventId: { _eq: 148 } }
+//         order_by: { createdAt: asc }
+//       ) {
+//         amount
+//         type
+//         createdAt
+//       }
+//     }
+//     event_user(
+//       where: { userId: { _eq: 3634 }, eventId: { _eq: 303 } }
+//     ) {
+//       level
+//     }
+//   }
+// `;
